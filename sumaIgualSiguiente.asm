6027130180d6098e22c436f9df9768e31ef4eabf
Jean Carlos Alarcon <jeancarlosalarguez16@gmail.com>
2019-01-23 11:22:44 -0500

    Ejercicio 1

---
+++ sumaIgualSiguiente.asm
@@ -0,0 +1,5 @@
+;Ejercicio para verificar 
+;Se ingresaran 3 numeros 2, 3, 4
+;Si la suma de los dos primeros numeros es igual al tercero o
+;SI la suma de el primer numero y el ultimo numero es igual al segundo numero
+;Si la suma de los dos ultimos es igual al primero.
